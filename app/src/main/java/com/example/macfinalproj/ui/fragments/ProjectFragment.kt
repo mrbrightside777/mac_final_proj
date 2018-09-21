540da5a3020033730eec118b654091784dcbd088
Halcyon Ramirez <halcyonjuly7@gmail.com>
2018-09-21 02:44:31 -0400

    added new instance method

--- app/src/main/java/com/example/macfinalproj/ui/fragments/ProjectFragment.kt
+++ app/src/main/java/com/example/macfinalproj/ui/fragments/ProjectFragment.kt
@@ -3,5 +3,11 @@ package com.example.macfinalproj.ui.fragments
 import androidx.fragment.app.Fragment
 
 class ProjectFragment: Fragment() {
+    companion object {
 
+        fun newInstance():Fragment {
+            val frag = ProjectFragment()
+            return frag
+        }
+    }
 }
