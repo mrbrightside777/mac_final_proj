ea6e810bfe620c98ef9ab8e5ff993352a7f2ad20
Halcyon Ramirez <halcyonjuly7@gmail.com>
2018-09-21 02:42:22 -0400

    made changes to preference screen logic

--- app/src/main/java/com/example/macfinalproj/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/macfinalproj/ui/fragments/SettingsFragment.kt
@@ -1,16 +1,63 @@
 package com.example.macfinalproj.ui.fragments
 
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import androidx.core.content.SharedPreferencesCompat
 import androidx.fragment.app.Fragment
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
 import com.example.macfinalproj.R
 
 
-class SettingsFrag: PreferenceFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.preferences)
+class SettingsFragment: PreferenceFragmentCompat() {
+
+    private lateinit var shared_prefs:SharedPreferences
+    private lateinit var editor:SharedPreferences.Editor
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+        shared_prefs = preferenceManager.sharedPreferences
+        editor = shared_prefs.edit()
+        setup_lang()
+        setup_theme()
+    }
+
+    private fun setup_lang() {
+
+        val lang_pref = findPreference("language") as ListPreference
+        lang_pref.setDefaultValue(shared_prefs.getString("language", "English"))
+
+        lang_pref.setOnPreferenceChangeListener {
+            preference, newValue ->
+            editor.putString(preference.key, newValue.toString())
+            editor.apply()
+            activity?.recreate()
+            true
+        }
+    }
+
+    private fun setup_theme() {
+        val theme_pref = findPreference("theme") as ListPreference
+        theme_pref.setDefaultValue(shared_prefs.getString("theme", "Light"))
+
+        theme_pref.setOnPreferenceChangeListener {
+            preference, newValue ->
+            editor.putString(preference.key, newValue.toString())
+            editor.apply()
+            activity?.recreate()
+            true
+        }
 
     }
+
+    companion object {
+        fun newInstance():Fragment {
+            val frag = SettingsFragment()
+            return frag
+        }
+    }
+
+
 }
