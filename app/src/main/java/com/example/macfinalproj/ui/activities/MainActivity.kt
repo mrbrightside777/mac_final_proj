d103dc52e7de8bc6d45096d633a48aac6749e871
luis1357 <luis.enriquez1357@gmail.com>
2018-09-21 02:30:06 -0400

    LoginMaster_Working

--- app/src/main/java/com/example/macfinalproj/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/macfinalproj/ui/activities/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.example.macfinalproj.ui.activities
 
-<<<<<<< HEAD
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-=======
 import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
->>>>>>> LoginV0.1
 import com.example.macfinalproj.R
 import com.example.macfinalproj.databinding.ActivityMainBinding
 import com.example.macfinalproj.utils.Constants
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity()
 {
@@ -38,17 +27,15 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initializeFirebaseComponents()
+        startFirebaseSignIn()
     }
 
     fun evt(view: View){
         val intent: Intent = Intent(this, ProjectsActivity::class.java)
         startActivity(intent)
-=======
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initializeFirebaseComponents()
-        startFirebaseSignIn()
+
     }
 
     override fun onResume()
@@ -117,6 +104,5 @@ class MainActivity : AppCompatActivity()
     private fun Any?.isNotNull(): Boolean
     {
         return this != null
->>>>>>> LoginV0.1
     }
 }
