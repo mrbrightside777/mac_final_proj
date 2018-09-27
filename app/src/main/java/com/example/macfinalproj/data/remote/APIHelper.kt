1ea0ed4a6ec5c7531f19459f7f1b2a76d4a8a9c2
Halcyon Ramirez <halcyonjuly7@gmail.com>
2018-09-27 10:55:14 -0400

    fixed api service helper

--- app/src/main/java/com/example/macfinalproj/data/remote/APIHelper.kt
+++ app/src/main/java/com/example/macfinalproj/data/remote/APIHelper.kt
@@ -1,16 +1,19 @@
 package com.example.macfinalproj.data.remote
 
+import com.example.macfinalproj.data.remote.pojo.Invites
+import com.example.macfinalproj.data.remote.pojo.Projects
+import com.example.macfinalproj.data.remote.pojo.Status
 import com.example.macfinalproj.di.components.DaggerRemoteServiceComponent
 import com.example.macfinalproj.di.modules.RetrofitModule
 import com.example.macfinalproj.utils.Constants
 import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import javax.inject.Inject
 
 
-class RemoteServiceHelper {
+class APIHelper {
     @Inject
-    lateinit var rest_api:RemoteService
+    lateinit var rest_api:APIService
 
     init {
         DaggerRemoteServiceComponent.builder()
@@ -20,10 +23,44 @@ class RemoteServiceHelper {
     }
 
     fun uploadProfPic() {
-        launch(context = CommonPool) {
+        runBlocking(context = CommonPool) {
 //            rest_api.changeProfPic().await()
         }
 
     }
 
+    fun save_project() {
+    }
+
+
+    fun getInvites(user_id:Int):Invites {
+        return runBlocking(context = CommonPool) {
+            rest_api.getInvites(user_id).await()
+        }
+    }
+
+    fun answerInvite(invite_id:Int, response:Char):Status {
+        return runBlocking(context = CommonPool) {
+            val body = mutableMapOf<String, Any>()
+            body["response"] = response
+            body["invite_id"] = invite_id
+            return@runBlocking rest_api.answerInvite(body).await()
+        }
+    }
+
+    fun createProject(project_owner:Int, project_name:String, project_description:String): Status {
+        return runBlocking(context = CommonPool) {
+            val body = mutableMapOf<String, Any>()
+            body["project_owner"] = project_owner
+            body["project_name"] = project_name
+            body["project_description"] = project_description
+            rest_api.createProject(body).await()
+        }
+    }
+
+    fun getProjects(owner:Int):Projects {
+        return runBlocking(context = CommonPool) {
+            rest_api.getProjects(owner).await()
+        }
+    }
 }
--- app/src/main/java/com/example/macfinalproj/data/remote/APIService.kt
+++ app/src/main/java/com/example/macfinalproj/data/remote/APIService.kt
@@ -3,14 +3,17 @@ package com.example.macfinalproj.data.remote
 import com.example.macfinalproj.data.models.pojo.ProfPic
 import com.example.macfinalproj.data.models.pojo.Project
 import com.example.macfinalproj.data.models.pojo.UserInfo
+import com.example.macfinalproj.data.remote.pojo.Invites
+import com.example.macfinalproj.data.remote.pojo.Projects
+import com.example.macfinalproj.data.remote.pojo.Status
 import com.example.macfinalproj.utils.Constants
 import kotlinx.coroutines.experimental.Deferred
 import okhttp3.MultipartBody
 import retrofit2.http.*
 
-interface RemoteService {
+interface APIService {
     @FormUrlEncoded
-    @POST("create_user")
+    @POST("create_user/")
     fun createUser(@Field("username")
                    username: String,
                    @Field("github_id")
@@ -29,12 +32,29 @@ interface RemoteService {
                   key: String=Constants.WEB.key): Deferred<UserInfo>
 
 
-    @GET("get_projects/")
-    fun getProjects(@Query("user_id")
-                    user_id: Int): Deferred<Project>
+    @GET("project/get_projects/")
+    fun getProjects(@Query("owner")
+                    owner: Int,
+                    @Query("key") key: String=Constants.WEB.key): Deferred<Projects>
 
     @Multipart
     @POST("change_prof_pic/")
     fun changeProfPic(@Part file: MultipartBody.Part): Deferred<ProfPic>
+
+
+    @Headers("Content-Type: application/json")
+    @POST("project/create_project/")
+    fun createProject(@Body body:MutableMap<String, Any>,  @Query("key") key: String=Constants.WEB.key): Deferred<Status>
+
+
+    @POST("project/invite_user/")
+    fun invite_user(invitor:Int, invitee:Int, project_id:Int):Deferred<Status>
+
+    @GET("user/get_invites/")
+    fun getInvites(@Query("user") user_id:Int, @Query("key") key: String=Constants.WEB.key):Deferred<Invites>
+
+    @Headers("Content-Type: application/json")
+    @POST("user/answer_invite/")
+    fun answerInvite(@Body answer:MutableMap<String, Any>): Deferred<Status>
 
 }
