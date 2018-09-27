ee5750ea8e5734d134fb3463e50e1a0c7628bfc8
Halcyon Ramirez <halcyonjuly7@gmail.com>
2018-09-27 10:54:38 -0400

    fixed repository

--- app/src/main/java/com/example/macfinalproj/data/repository/Repository.kt
+++ app/src/main/java/com/example/macfinalproj/data/repository/Repository.kt
@@ -1,4 +1,17 @@
 package com.example.macfinalproj.data.repository
 
+
+import com.example.macfinalproj.data.remote.pojo.Invites
+import com.example.macfinalproj.data.remote.pojo.Projects
+import com.example.macfinalproj.data.remote.pojo.Status
+
 interface Repository {
+
+
+    fun saveProject(project_name: String, project_description:String)
+    fun saveTask()
+    fun getInvites(user_id:Int): Invites
+    fun answerInvite(invite_id:Int, response:Char): Status
+    fun createProject(project_owner:Int, project_name: String, project_description: String):Status
+    fun getProjects(user_id: Int):Projects
 }
